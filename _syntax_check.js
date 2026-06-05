c7a8834f1388968546ba83d19fbe20cce42bd642
Omar <omar.quraiishi@gmail.com>
2026-06-05 20:11:56 +0300

    chore: clean local artifacts and static links

--- _syntax_check.js
+++ _syntax_check.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
+const vm = require('vm');
 const html = fs.readFileSync('static/index-v2-prototype.html', 'utf8');
 const blocks = html.match(/<script[^>]*>([\s\S]*?)<\/script>/g) || [];
-let braces = 0, parens = 0, bt = false;
-blocks.forEach(b => {
-  const js = b.replace(/<script[^>]*>/, '').replace(/<\/script>/, '');
-  for(let c of js) {
-    if(c === '{') braces++; if(c === '}') braces--;
-    if(c === '(') parens++; if(c === ')') parens--;
-    if(c === '`') bt = !bt;
-  }
+let all = '';
+blocks.forEach((b) => {
+  all += b.replace(/<script[^>]*>/, '').replace(/<\/script>/, '') + '\n';
 });
-console.log('Braces:', braces, 'Parens:', parens, 'Backtick:', bt);
-if(braces !== 0 || parens !== 0 || bt) process.exit(1);
+
+try {
+  new vm.Script(all, { filename: 'static/index-v2-prototype.html <script>' });
+} catch (error) {
+  console.error(error && error.stack ? error.stack : error);
+  process.exit(1);
+}
+
 console.log('JS syntax OK');
